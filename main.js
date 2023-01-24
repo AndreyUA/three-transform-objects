c2bc679050b059d15d439e2313256cbd3190b621
AndreyUA <infernalsteel666@gmail.com>
2023-01-24 22:33:02 +0200

    scale objects

--- main.js
+++ main.js
@@ -6,22 +6,26 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshMatcapMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 // Change position just for fun
 // mesh.position.x = 0.7;
 // mesh.position.y = -0.6;
 // mesh.position.z = 1;
+mesh.position.set(0.7, -0.6, 1);
 
 // mesh.position.normalize() --> this will normalize position to vector length 1
 
-mesh.position.set(0.7, -0.6, 1);
+// Scaling
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.z = 0.5;
+mesh.scale.set(2, 0.5, 0.5);
 
 console.log(
   "vector3 length (between the center of scene and position): ",
   mesh.position.length()
 );
-
-scene.add(mesh);
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper(3);
