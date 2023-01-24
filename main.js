e8de74f89000ddd5edd06ad9bd09f8bd947fa95e
AndreyUA <infernalsteel666@gmail.com>
2023-01-24 22:43:20 +0200

    rotate objects

--- main.js
+++ main.js
@@ -22,6 +22,10 @@ mesh.position.set(0.7, -0.6, 1);
 // mesh.scale.z = 0.5;
 mesh.scale.set(2, 0.5, 0.5);
 
+// Rotation
+mesh.rotation.x = Math.PI * 0.25;
+mesh.rotation.y = Math.PI * 0.25;
+
 console.log(
   "vector3 length (between the center of scene and position): ",
   mesh.position.length()
