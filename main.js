e622f8b2823b46d01b1e21aba4b66069a0f16f84
AndreyUA <infernalsteel666@gmail.com>
2023-01-24 22:15:29 +0200

    some positions features

--- main.js
+++ main.js
@@ -6,12 +6,18 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshMatcapMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
 
 // Change position just for fun
 mesh.position.x = 0.7;
 mesh.position.y = -0.6;
 mesh.position.z = 1;
+
+console.log(
+  "vector3 length (between the center of scene and position): ",
+  mesh.position.length()
+);
+
+scene.add(mesh);
 
 const size = {
   width: 800,
@@ -21,6 +27,11 @@ const size = {
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 camera.position.z = 3;
 scene.add(camera);
+
+console.log(
+  "distance between cube and camera: ",
+  mesh.position.distanceTo(camera.position)
+);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("canvas.webgl"),
